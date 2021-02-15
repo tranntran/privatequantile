d7088ddb743a4a87c7d45aaa382c4189ebc22d54
Tran Tran <ttran2401@gmail.com>
2021-02-15 01:40:21 -0500

    correct error in design matrix

--- code/simulations.R
+++ code/simulations.R
@@ -1,4 +1,4 @@
-source("functions.R")
+source("code/functions.R")
 
 utility_logit = function(data, inds){
   data = cbind(data, inds)
@@ -17,15 +17,16 @@ utility_logit_inter = function(data, inds){
 }
 
 syndata = function(beta_result, x, select_quantile){
-  print(head(select_quantile))
+#  print(head(select_quantile))
   allsyn = x%*%beta_result
   coord = cbind(c(1:nrow(x)), select_quantile)
   ans = allsyn[coord]
   return(ans)
 }
 
-originalKNG = function(data, total_eps, tau, nbatch = 1000, scale = 0, start_scale = 0.001,
-                       lower_accept = 0.2, upper_accept = 0.25, nonneg = FALSE){
+originalKNG = function(data, total_eps, tau, nbatch = 1000, scale = rep(0, length(tau)), 
+                       start_scale = 0.001, lower_accept = 0.2, upper_accept = 0.25, 
+                       nonneg = FALSE){
   ep = total_eps/length(tau)
   scale_kng = 0
   i = ncol(data)
@@ -37,16 +38,22 @@ originalKNG = function(data, total_eps, tau, nbatch = 1000, scale = 0, start_sca
   sumX = apply(X = X, 2, FUN = sum)
   m = ncol(X) - 1
   
+  accept_rate = rep(NA, length(tau))
+  scale_output = rep(NA, length(tau))
   ans = NA
   for (i in 1:length(tau)){
     print(tau[i])
-    ans = cbind(ans, KNG(ep = ep, tau = tau[i], sumX = sumX, X = X, Y = Y, 
-              nbatch = nbatch, scale = scale, start_scale = start_scale, 
-              upper_accept = upper_accept, lower_accept = lower_accept, 
-              nonneg = nonneg)[[1]]*R)
+    curr_scale = scale[i]
+    temp = KNG(ep = ep, tau = tau[i], sumX = sumX, X = X, Y = Y, 
+               nbatch = nbatch, scale = curr_scale, start_scale = start_scale, 
+               upper_accept = upper_accept, lower_accept = lower_accept, 
+               nonneg = nonneg)
+    ans = cbind(ans, temp[[1]]*R)
+    scale_output[i] = temp[[2]]
+    accept_rate[i] = temp[[3]]
   }
   ans = ans[, -1]
-  return(ans)
+  return(list(ans, scale_output, accept_rate))
 }
 
 # beta_res = list()
@@ -61,12 +68,13 @@ originalKNG = function(data, total_eps, tau, nbatch = 1000, scale = 0, start_sca
 library(quantreg)
 library(reshape2)
 library(ggplot2)
-set.seed(2021)
-reps = 2
+t = 1
+set.seed(t)
+reps = 1000
 ut_logit = matrix(NA, nrow = 6, ncol = reps)
 ut_logit_inter = matrix(NA, nrow = 6, ncol = reps)
-main_tau = c(0.05, 0.25, 0.5, 0.75, 0.95, 0.99)
-tau = c(seq(0.05, 0.95, 0.05), 0.99)
+main_tau = c(0.05, 0.25, 0.5, 0.75, 0.95)
+tau = c(seq(0.05, 0.95, 0.05))
 
 ep = 0.25
 n = 5000
@@ -82,12 +90,19 @@ b2 = 1
 #why ggplot is not plotting
 #what is this error No id variables; using all as measure variables
 par(mfrow = c(1,1))
+scale_x1 = rep(list(c(rep(0.01, 10), rep(0.03, 9))), 5)
+accept_x1 = rep(list(rep(0, length(tau))), 5)
+scale_x2 = rep(list(c(rep(0.0005, 9), 0.001, rep(0.0005, 9))), 5)
+accept_x2 = rep(list(rep(0, length(tau))), 5)
+scale_x3 = rep(list(c(rep(0.0001, 10), rep(0.0001, 9))), 5)
+accept_x3 = rep(list(rep(0, length(tau))), 5)
 for (j in 1:reps){
   print(j)
   x1 = rexp(n, lambda)
   x2 = a0 + b0*x1 + rexp(n, lambda)
   x3 = a1 + b1*x1 + b2*x2 + rexp(n, lambda)
   vars = c("x1", "x2", "x3")
+  #vars = c("x1", "x2")
   
   for (k in 1:length(vars)){
     syn_var = vars[k]
@@ -95,34 +110,56 @@ for (j in 1:reps){
     if (syn_var == "x1"){
       data = as.data.frame(x1)
       all_beta = list()
-      all_beta[[1]] = originalKNG(data = data, total_eps = ep, tau = tau, start_scale = 0.01)
-      # 
-      # all_beta[[2]] = stepwiseKNG(data = data, total_eps = ep, median_eps = 1/length(tau), tau = tau,
-      #                             nbatch = runs, method = "koenker", nonneg = TRUE)[[1]]
-      # all_beta[[3]] = stepwiseKNG(data = data, total_eps = ep, median_eps = 1/length(tau), tau = tau,
-      #                             nbatch = runs, method = "currentdata", nonneg = TRUE)[[1]]
-      # all_beta[[4]] = sandwichKNG(data = data, total_eps = ep, median_eps = 1/length(main_tau), 
-      #                             main_tau_eps = length(main_tau)/length(tau),
-      #                             tau = tau, main_tau = main_tau, nbatch = runs, 
-      #                             method = "koenker", nonneg = TRUE)[[1]]
-      # all_beta[[5]] = sandwichKNG(data = data, total_eps = ep, median_eps = 1/length(main_tau), 
-      #                             main_tau_eps = length(main_tau)/length(tau),
-      #                             tau = tau, main_tau = main_tau, nbatch = runs, 
-      #                             method = "currentdata", nonneg = TRUE)[[1]]
-      # all_beta[[6]] = coef(rq(x1 ~ 1, tau))
+      temp = originalKNG(data = data, total_eps = ep, tau = tau, start_scale = 0.01,
+                                  scale = scale_x1[[1]])
+      all_beta[[1]] = temp[[1]]
+      scale_x1[[1]] = temp[[2]]
+      accept_x1[[1]] = temp[[3]]
+
+      temp = stepwiseKNG(data = data, total_eps = ep, median_eps = 1/length(tau), 
+                         tau = tau, nbatch = runs, method = "koenker", nonneg = TRUE,
+                         scale = scale_x1[[2]])
+      all_beta[[2]] = temp[[1]]
+      scale_x1[[2]] = temp[[2]]
+      accept_x1[[2]] = temp[[3]]
+      
+      temp = stepwiseKNG(data = data, total_eps = ep, median_eps = 1/length(tau), 
+                         tau = tau, nbatch = runs, method = "currentdata", 
+                         nonneg = TRUE, scale_x1[[3]])
+      all_beta[[3]] = temp[[1]]
+      scale_x1[[3]] = temp[[2]]
+      accept_x1[[3]] = temp[[3]]
+      
+      temp = sandwichKNG(data = data, total_eps = ep, median_eps = 1/length(main_tau),
+                                  main_tau_eps = length(main_tau)/length(tau),
+                                  tau = tau, main_tau = main_tau, nbatch = runs,
+                                  method = "koenker", nonneg = TRUE, scale = scale_x1[[4]])
+      all_beta[[4]] = temp[[1]]
+      scale_x1[[4]] = temp[[2]]
+      accept_x1[[4]] = temp[[3]]
+      
+      
+      temp = sandwichKNG(data = data, total_eps = ep, median_eps = 1/length(main_tau),
+                                  main_tau_eps = length(main_tau)/length(tau),
+                                  tau = tau, main_tau = main_tau, nbatch = runs,
+                                  method = "currentdata", nonneg = TRUE, scale = scale_x1[[5]])
+      all_beta[[5]] = temp[[1]]
+      scale_x1[[5]] = temp[[2]]
+      accept_x1[[5]] = temp[[3]]
+      all_beta[[6]] = coef(rq(x1 ~ 1, tau))
       
       
       
-      #X = rep(list(matrix(1, nrow = n)), 6)
-      X = rep(list(matrix(1, nrow = n)), 1)
+      X = rep(list(matrix(1, nrow = n)), 6)
+      #X = rep(list(matrix(1, nrow = n)), 1)
       synx1 = mapply(syndata, beta_result = all_beta, x = X, 
                    MoreArgs = list(sample(1:length(tau), n, replace = TRUE)), SIMPLIFY = FALSE)
       synall = synx1
-      #synx1[[7]] = x1
-      # names(synx1) = c("Original", "Stepwise-Koenker", "Stepwise-Data", "Sandwich-Koenker", "Sandwich-Data",
-      #                "Non-Private", "Truth")
-      synx1[[2]] = x1
-      names(synx1) = c("Original", "Truth")
+      synx1[[7]] = x1
+      names(synx1) = c("Original", "Stepwise-Koenker", "Stepwise-Data", 
+                       "Sandwich-Koenker", "Sandwich-Data", "Non-Private", "Truth")
+      # synx1[[2]] = x1
+      # names(synx1) = c("Original", "Truth")
       plotdata = melt(synx1)
       plotdata$L1 = as.factor(plotdata$L1)
       print(ggplot(plotdata, aes(x=value, fill = L1)) + facet_wrap(~L1, ncol = 4) +
@@ -140,30 +177,77 @@ for (j in 1:reps){
         mod = "x3 ~ x1 + x2"
       }
       all_beta = list()
-      all_beta[[1]] = originalKNG(data = data, total_eps = ep, tau = tau)
-      # 
-      # all_beta[[2]] = stepwiseKNG(data = data, total_eps = ep, median_eps = 0.5, tau = tau,
-      #                             nbatch = runs, method = "koenker", nonneg = TRUE)[[1]]
-      # all_beta[[3]] = stepwiseKNG(data = data, total_eps = ep, median_eps = 0.6, tau = tau,
-      #                             nbatch = runs, method = "newdata", check_data = synall[[3]],
-      #                             nonneg = TRUE)[[1]]
-      # all_beta[[4]] = sandwichKNG(data = data, total_eps = ep, median_eps = 0.5, main_tau_eps = 0.7,
-      #                             tau = tau, main_tau = main_tau, nbatch = runs, 
-      #                             method = "koenker", nonneg = TRUE)[[1]]
-      # all_beta[[5]] = sandwichKNG(data = data, total_eps = ep, median_eps = 0.6, main_tau_eps = 0.5,
-      #                             tau = tau, main_tau = main_tau, nbatch = runs, 
-      #                             method = "newdata", check_data = synall[[5]], nonneg = TRUE)[[1]]
-      # all_beta[[6]] = coef(rq(mod, tau))
-      #X = mapply(cbind, synall, rep(list(matrix(1, nrow = n)), 6), SIMPLIFY = FALSE)
-      X = mapply(cbind, synall, rep(list(matrix(1, nrow = n)), 1), SIMPLIFY = FALSE)
+      temp = originalKNG(data = data, total_eps = ep, tau = tau, 
+                         scale = if(syn_var == "x2") scale_x2[[1]] else scale_x3[[1]])
+      all_beta[[1]] = temp[[1]]
+      if (syn_var == "x2") {
+        scale_x2[[1]] = temp[[2]]
+        accept_x2[[1]] = temp[[3]]
+      } else {
+        scale_x3[[1]] = temp[[2]]
+        accept_x3[[1]] = temp[[3]]
+      }
+
+      temp = stepwiseKNG(data = data, total_eps = ep, median_eps = 0.5, tau = tau,
+                         nbatch = runs, method = "koenker", nonneg = TRUE,
+                         scale = if(syn_var == "x2") scale_x2[[2]] else scale_x3[[2]])
+      all_beta[[2]] = temp[[1]]
+      if (syn_var == "x2") {
+        scale_x2[[2]] = temp[[2]]
+        accept_x2[[2]] = temp[[3]]
+      } else {
+        scale_x3[[2]] = temp[[2]]
+        accept_x3[[2]] = temp[[3]]
+      }
+      
+      temp = stepwiseKNG(data = data, total_eps = ep, median_eps = 0.6, tau = tau,
+                         nbatch = runs, method = "newdata", check_data = synall[[3]],
+                         nonneg = TRUE, scale = if(syn_var == "x2") scale_x2[[3]] else scale_x3[[3]])
+      all_beta[[3]] = temp[[1]]
+      if (syn_var == "x2") {
+        scale_x2[[3]] = temp[[2]]
+        accept_x2[[3]] = temp[[3]]
+      } else {
+        scale_x3[[3]] = temp[[2]]
+        accept_x3[[3]] = temp[[3]]
+      }
+      
+      temp = sandwichKNG(data = data, total_eps = ep, median_eps = 0.5, main_tau_eps = 0.7,
+                         tau = tau, main_tau = main_tau, nbatch = runs, method = "koenker", 
+                         nonneg = TRUE, scale = if(syn_var == "x2") scale_x2[[4]] else scale_x3[[4]])
+      all_beta[[4]] = temp[[1]]
+      if (syn_var == "x2") {
+        scale_x2[[4]] = temp[[2]]
+        accept_x2[[4]] = temp[[3]]
+      } else {
+        scale_x3[[4]] = temp[[2]]
+        accept_x3[[4]] = temp[[3]]
+      }
+      
+      temp = sandwichKNG(data = data, total_eps = ep, median_eps = 0.6, main_tau_eps = 0.5,
+                         tau = tau, main_tau = main_tau, nbatch = runs, method = "newdata", 
+                         check_data = synall[[5]], nonneg = TRUE,
+                         scale = if(syn_var == "x2") scale_x2[[5]] else scale_x3[[5]])
+      all_beta[[5]] = temp[[1]]
+      if (syn_var == "x2") {
+        scale_x2[[5]] = temp[[2]]
+        accept_x2[[5]] = temp[[3]]
+      } else {
+        scale_x3[[5]] = temp[[2]]
+        accept_x3[[5]] = temp[[3]]
+      }
+      
+      all_beta[[6]] = coef(rq(mod, tau))
+      X = mapply(cbind, rep(list(matrix(1, nrow = n)), 6), synall, SIMPLIFY = FALSE)
+      #X = mapply(cbind, synall, rep(list(matrix(1, nrow = n)), 1), SIMPLIFY = FALSE)
       syn = mapply(syndata, beta_result = all_beta, x = X, 
                      MoreArgs = list(sample(1:length(tau), n, replace = TRUE)), SIMPLIFY = FALSE)
       synall = mapply(cbind, synall, syn, SIMPLIFY = FALSE)
-      #syn[[7]] = data[, ncol(data)]
-      # names(syn) = c("Original", "Stepwise-Koenker", "Stepwise-Data", "Sandwich-Koenker", 
-      #                  "Sandwich-Data", "Non-Private", "Truth")
-      syn[[2]] = data[, ncol(data)]
-      names(syn) = c("Original", "Truth")
+      syn[[7]] = data[, ncol(data)]
+      names(syn) = c("Original", "Stepwise-Koenker", "Stepwise-Data", "Sandwich-Koenker",
+                       "Sandwich-Data", "Non-Private", "Truth")
+      # syn[[2]] = data[, ncol(data)]
+      # names(syn) = c("Original", "Truth")
       plotdata = melt(syn)
       plotdata$L1 = as.factor(plotdata$L1)
       print(ggplot(plotdata, aes(x=value, fill = L1)) + facet_wrap(~L1, ncol = 4) +
@@ -186,12 +270,15 @@ for (j in 1:reps){
   ut_logit_inter[, j] = sapply(ut.data, utility_logit_inter, inds)
 }
 
+filename = paste("output/utility_", t, ".Rdata", sep = "")
 
 tab1 = rbind(apply(ut_logit, 1, median), apply(ut_logit_inter, 1, median))
 rownames(tab1) = c("Utility Logit", "Utility Logit Inter")
 colnames(tab1) = c("Original", "Stepwise-Koenker", "Stepwise-Data", "Sandwich-Koenker", "Sandwich-Data",
                    "Non-Private")
 tab1
+
+save(list = c("ut_logit", "ut_logit_inter", "tab1"), file = filename)
 
 
 tab2 = rbind(apply(ut_logit, 1, mean), apply(ut_logit_inter, 1, mean))
