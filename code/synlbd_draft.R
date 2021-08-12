bbd22bba118a80fa035bc6f1c4a56c2321aafab8
Tran Tran <ttran2401@gmail.com>
2021-08-12 01:03:50 -0400

    update synlbd draft

--- code/synlbd_draft.R
+++ code/synlbd_draft.R
@@ -7,9 +7,22 @@ dat = as.data.table(dat)
 
 
 product_col = function(data, col1, col2){
-  data = as.data.table(dat)
+  data = as.data.table(data)
   
   if (length(col1) == 0 | length(col2) == 0){
     return(data)
   }
+  vars = paste(col1, col2, sep = "*")
+  data[, (vars) := lapply(.SD, function(x) x*data[[col1]]), .SDcols = col2]
+  
+  return(data)
 }
+
+dat1 = product_col(dat, col1, col2)
+
+DT <- data.table(id=1:1000,year=round(runif(1000)*10), 
+                 inc1 = runif(1000), inc2 = runif(1000), inc3 = runif(1000),    
+                 deflator = rnorm(1000))
+inc_cols = c('inc1', 'inc2','inc3')
+DT[, (inc_cols) := lapply(.SD, function(x) 
+  x * DT[['deflator']] ), .SDcols = inc_cols]
